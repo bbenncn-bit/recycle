fb2898540be14cb29a570003f22f698a7be29daf
蔡韬 <oncwnuHyCuY1sF9CTZpqNY18cNJc@git.weixin.qq.com>
2026-04-20 18:33:19 +0800

    update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,20 @@
 module.exports = {
-    apps: [
-      {
-        name: 'pxrecycle',
-        cwd: '/home/ubuntu/pxrecycle/current',
-        script: 'node_modules/next/dist/bin/next',
-        args: 'start -p 3000',
-        env: {
-          NODE_ENV: 'production'
-        }
+  apps: [
+    {
+      name: "pxrecycle",
+      cwd: __dirname,
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_restarts: 20,
+      min_uptime: "10s",
+      restart_delay: 3000,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production"
       }
-    ]
-  };
+    }
+  ]
+};
